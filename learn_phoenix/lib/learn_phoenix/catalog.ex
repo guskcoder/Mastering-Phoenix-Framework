bed326d61f2c5401b21bc6ad0be4d9f4a7534cc9
guskcoder <gustavo@imaginative.company>
2021-06-29 07:39:26 -0300

    Function Random finish

--- learn_phoenix/lib/learn_phoenix/catalog.ex
+++ learn_phoenix/lib/learn_phoenix/catalog.ex
@@ -3,7 +3,12 @@ defmodule Catalog do
     ["Tomato", "Apple", "Potato"]
   end
 
-  def random_product(list) do
+  def random_product(list) when is_list(list) do
     Enum.random(list)
   end
+
+  def random_product(_), do: {:error, "Not a list"}
+
+  def random_product, do: {:error, "Need a List of Product"}
+
 end
